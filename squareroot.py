3061f98143e68acfa26da0efd184a78b3233721d
Alfred P Benjamin <146985217+Alfy001@users.noreply.github.com>
2025-01-20 19:41:31 +0530

    square_root

--- squareroot.py
+++ squareroot.py
@@ -1,7 +1,15 @@
-num = float(input("Enter the number: "))
+num_input = int(input("Enter a number: "))
+approx_root = 1
 
-if num < 0:
-    print("Square root of negative number is complex:", complex(0, (-num) ** 0.5))
+if num_input < 0:
+    print("Square root of a negative number is not a real number.")
 else:
-    sqrroot = num ** 0.5
-    print("Square Root:", sqrroot)
+    for i in range(num_input):
+        if num_input == (approx_root * approx_root):
+            break
+        approx_root += 1
+    approx_root = (approx_root + num_input / approx_root) / 2
+    error = abs(num_input - approx_root ** 2)
+
+    if error == 0:
+        print("The Square root 0f", num_input, "=", approx_root)
